691e896aed07350db947af52e661cad9433ec44a
Rich Costello <richcostello68@gmail.com>
2016-06-15 08:30:59 -0700

    first commit
    barebones webpack react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var App = React.createClass({
+  
+
+  render() {
+    return (
+      <div>
+     <h1>Barebones React Webapp</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
